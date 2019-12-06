a080d9ba5042e1c554a17ebe96618f3aed763871
zabirtech <sabirquazi@gmail.com>
2019-12-06 13:10:26 +0100

    Main dart file
    i am rich app

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
